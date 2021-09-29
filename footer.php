12c93223c7e0313d68d703d221e3f2c2a9cf6178
sabrine17 <78363555+sabrine17@users.noreply.github.com>
2021-09-29 14:24:51 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package time1
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+<div>
+	<!--	<div class="site-info">
+		
+		</div>--><!-- .site-info -->
+
+		<img class="logoM9" > </img>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
